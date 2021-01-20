ab95b0f5ebe5beb88dde02a28ecd0571b48f7133
mdporter <mportertime@gmail.com>
2021-01-20 18:43:23 -0500

    add functions to tidy arules::apriori objects

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,41 @@
 #' @return A character vector of the number rounded to k decimal places.
 #' @export
 digits <- function(x, k=2) format(round(x, digits=k), nsmall=k)
+
+
+
+#' Tidy an [arules::apriori()] object
+#'
+#' Converts output from [arules::apriori()] to a tibble/dataframe.
+#'
+#' Use this instead of [arules::inspect()], which only prints to screen.
+#'
+#' @param x An object of class [arules::rules] or [arules::itemsets] produced
+#'        by running [arules::apriori()]
+#' @return a tibble (or data.frame) of the rules/itemsets
+#' @export
+apriori2df <- function(x){
+  if(class(x) == "itemsets")   tidy.itemsets(x)
+  else if(class(x) == "rules") tidy.rules(x)
+  else stop("Only works with class of itemsets or rules")
+}
+
+
+
+#' @describeIn apriori2df Tidy a [arules::rules] object
+#' @export
+tidy.rules <- function(x){
+  out = data.frame(
+    lhs = arules::labels(x@lhs),
+    rhs = arules::labels(x@rhs),
+    x@quality,
+    stringsAsFactors = FALSE)
+  if(requireNamespace(tibble::tibble)) tibble::as_tibble(out) else out
+}
+
+#' @describeIn apriori2df Tidy a [arules::itemsets] object
+#' @export
+tidy.itemsets <- function(x){
+  out = data.frame(items=arules::labels(x), x@quality, stringsAsFactors = FALSE)
+  if(requireNamespace(tibble::tibble)) tibble::as_tibble(out) else out
+}
