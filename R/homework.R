fab8794f766c81822e35d4f3c5878ce43cd312d0
mdporter <mportertime@gmail.com>
2021-02-12 17:17:29 -0500

    change df_print

--- R/homework.R
+++ R/homework.R
@@ -3,7 +3,7 @@
 #' @param nb (logical) should an Rnotebook or html document be used
 #' @seealso \url{https://rstudio.github.io/rstudio-extensions/rmarkdown_templates.html#custom-formats}
 #' @export
-homework <- function(nb = FALSE) {
+homework <- function(nb = FALSE, ...) {
   css = system.file("config/hw.css", package = "R6018")
   html_format <- ifelse(nb, rmarkdown::html_notebook, rmarkdown::html_document)
   html_format(toc = TRUE
@@ -11,6 +11,6 @@ homework <- function(nb = FALSE) {
              ,toc_depth = 2
              ,theme = "default"
              ,css = css
-             ,df_print = pander::pander
-             ,pandoc_args= "--strip-comments")
+             ,df_print = "paged"
+             ,pandoc_args= "--strip-comments", ...)
 }
